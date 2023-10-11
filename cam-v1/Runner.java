357aec3dc40e69c4756dcf48e3210f985b602150
Noah Kim <noahhk25@ausdk12.org>
2023-10-11 11:34:35 -0700

    Virtual pet alonzo

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,30 @@
 public class Runner {
-    public static void main(String[] args) {
+    public Runner() {
         VirtualPet v = new VirtualPet();
-        v.feed();
+
+
         v.exercise();
+        takeABeat(1000);
+        v.sleep();
+        
+        
+        
+        // String ans = getAnswer("How are you?");
+        // if(ans.equalsIgnoreCase("Good")){
+        //     v.respondPositively();
+        // }
+    }
+    
+    
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){
+        
+        }
+
+    }
+    public static void main(String[] args) {
+        new Runner();
     }
 }
